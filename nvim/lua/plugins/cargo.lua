aa5faed9e73e026a908f49bdbc6c845f9d31cc1c
Ioscipescu <blountwy@gmail.com>
2025-08-13 15:47:05 -0600

    Fixed an issue with cargo

--- nvim/lua/plugins/cargo.lua
+++ nvim/lua/plugins/cargo.lua
@@ -1,15 +1,16 @@
 return {
   "nwiizo/cargo.nvim",
+  event = "BufRead Cargo.toml",
   build = "cargo build --release",
   config = function()
-    require("cargo").setup {
+    require("cargo").setup({
       float_window = true,
       window_width = 0.8,
       window_height = 0.8,
       border = "rounded",
       auto_close = true,
       close_timeout = 5000,
-    }
+    })
   end,
   ft = { "rust" },
   cmd = {
